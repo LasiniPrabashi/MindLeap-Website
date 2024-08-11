cd6962cc728fc8f3fcdcd2cd8ba85705a3c74655
LasiniPrabashi <lasiniprabashi0327@gmail.com>
2024-08-11 21:01:08 +0530

    mind leap mental website

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+import About from './component/About'
+import Hero from './component/Hero'
+import Navbar from './component/Navbar'
+import Services from './component/Services'
+import Testimonial from './component/Testimonial'
+import Pricing from './component/pricing'
+
+function App() {
+  
+
+  return (
+    <div className='font-primary overflow-x-hidden '>
+     <Navbar/>
+     <Hero/>
+     <Services/>
+     <About/>
+     <Pricing/>
+     <Testimonial/>
+    {/* <h1 className='text-2xl font-normal'>MindLeap Mentall Wellness React Project Starter Files</h1> */}
+   </div>
+  )
+}
+
+export default App
